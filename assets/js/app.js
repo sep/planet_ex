246dcdd746c9d1f0e163ccebe51bf3dd216ed06d
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2018-05-29 22:12:13 -0400

    Switch to using Yarn from NPM
    * deletes package-lock.json and create yarn.lock
    * cleaned up boilerplate code from socket.js and app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,15 +7,3 @@
 // in vendor, which are never wrapped in imports and
 // therefore are always executed.
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
