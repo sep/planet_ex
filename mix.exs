50079a5198292505d51d6baa816988f9ede36884
Mitchell Hanberg <mitch@mitchellhanberg.com>
2019-06-10 14:43:39 -0400

    Bump version to v0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlanetEx.Mixfile do
   def project do
     [
       app: :planetex,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
